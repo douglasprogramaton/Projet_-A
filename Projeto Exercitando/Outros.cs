06fa69d8b881ee899663942e2ea6d92ac798b65c
douglasprogramaton <douglassantosprogramaton@gmail.com>
2022-04-14 11:06:49 -0300

    Projet

---
+++ Projeto Exercitando/Outros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Exercitando
+{
+
+    public class Outros
+    {
+
+
+        public static List<string> armamentos = new List<string>();
+
+        public static List<int> preços = new List<int>();
+
+    }
+}
+
+
+
+
---
+++ Projeto Exercitando/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Exercitando
+{
+    class Guerra
+    {
+
+        public static int baixas;
+        public static string nsoldadobr;      //
+        public static string nsoldadofarc;   //
+        public static int idadeb;
+        public static int idadef;
+        public static int idadefarc;       //         ATRIBUTOS DA CLASS.
+        public static dynamic Items = new List<dynamic>();                        //
+        static void Start()
+        {
+
+           
+            List<string> soldadobr = new List<string>();
+            List<int> idadebr = new List<int>();
+            List<string> nomesfarc = new List<string>();
+            List<int> idadefarc = new List<int>();
+            List<string> armamentos = new List<string>();
+            string data = String.Format("{0:dd/MM/yyyy HH:mm:ss}", DateTime.Now);
+
+
+            Console.WriteLine("Inicio de uma Guerra No territorio amazonico ");
+            Console.WriteLine("=============================================");
+            Console.WriteLine($"Digite a quantidade de soldados Brasileiros");
+            int QuantidadeBR = int.Parse(Console.ReadLine());
+            List<int> Qtdesd = new List<int>();
+            for (int i = 0; i < QuantidadeBR; i++)
+            {
+               
+                Qtdesd.Add(QuantidadeBR + 1);
+
+
+                Console.WriteLine("Digite a Patente e o Nome do soldadoBR");
+                nsoldadobr = Console.ReadLine();
+                
+                Console.WriteLine("digite a idade ");
+                idadeb = int.Parse(Console.ReadLine());
+                idadebr.Add(idadeb);
+               
+                DateTime dataEntrada = DateTime.Now;
+                Console.WriteLine(dataEntrada);
+                Console.WriteLine("Data e horario de entrada na Guerra");
+            }
+            
+            Console.WriteLine("Inicio de uma Guerra No territorio amazonico ");
+            Console.WriteLine("=============================================");
+            Console.WriteLine($"Digite a quantidade de soldados das Forças Farc");
+            int Quantidadefarc = int.Parse(Console.ReadLine());
+            List<int> Qtdes = new List<int>();
+            for (int i = 0; i < Quantidadefarc; i++)
+            {
+
+                Qtdes.Add(Quantidadefarc + 1);
+
+                Console.WriteLine("Digite a Patente e o Nome do soldadoFarc");
+                nsoldadofarc = Console.ReadLine();
+                Console.WriteLine("digite a idade ");
+                idadef = int.Parse(Console.ReadLine());
+                idadefarc.Add(idadef);
+
+               ;
+
+            }
+        }
+        
+        static void Main()
+        {
+            Start();
+            
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
