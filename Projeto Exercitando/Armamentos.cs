97a3e7432fd0ca8c24e9980f207c8341701e9c06
douglasprogramaton <douglassantosprogramaton@gmail.com>
2022-04-16 11:26:26 -0300

    Projet

---
+++ Projeto Exercitando/Armamentos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Exercitando
+{
+    class Armamentos
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public string munição { get; set; }
+        public List<Armamentos> armamentos;
+        public List<Armamentos> ItemsArmas { get; set; }
+
+
+
+
+        Armamentos()
+        {
+            ItemsArmas = new List<Armamentos>();
+            armamentos.Add(new Armamentos { Id = 1, });
+        }
+    }
+}
+
+
+
+
+
--- Projeto Exercitando/Outros.cs
+++ Projeto Exercitando/Outros.cs
@@ -1,22 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Projeto_Exercitando
 {
-
-    public class Outros
+    class Guerra
     {
 
+        public static int baixas;
+        public static string nsoldadobr;      //
+        public static string nsoldadofarc;   //
+        public static int idadeb;
+        public static int idadef;
+        public static int idadefarc;       //         ATRIBUTOS DA CLASS.
+        public static dynamic Items = new List<dynamic>();                        //
+        public static void Start()
+        {
 
-        public static List<string> armamentos = new List<string>();
 
-        public static List<int> preços = new List<int>();
+            List<string> soldadobr = new List<string>();
+            List<int> idadebr = new List<int>();
+            List<string> nomesfarc = new List<string>();
+            List<int> idadefarc = new List<int>();
 
+            string data = String.Format("{0:dd/MM/yyyy HH:mm:ss}", DateTime.Now);
+
+
+            Console.WriteLine("Inicio de uma Guerra No territorio amazonico ");
+            Console.WriteLine("=============================================");
+            Console.WriteLine($"Digite a quantidade de soldados Brasileiros");
+            int QuantidadeBR = int.Parse(Console.ReadLine());
+            List<int> Qtdesd = new List<int>();
+            for (int i = 0; i < QuantidadeBR; i++)
+            {
+
+                Qtdesd.Add(QuantidadeBR + 1);
+
+
+                Console.WriteLine("Digite a Patente e o Nome do soldadoBR");
+                nsoldadobr = Console.ReadLine();
+
+                Console.WriteLine("digite a idade ");
+                idadeb = int.Parse(Console.ReadLine());
+                idadebr.Add(idadeb);
+
+                DateTime dataEntrada = DateTime.Now;
+                Console.WriteLine(dataEntrada);
+                Console.WriteLine("Data e horario de entrada na Guerra");
+            }
+
+            Console.WriteLine("Inicio de uma Guerra No territorio amazonico ");
+            Console.WriteLine("=============================================");
+            Console.WriteLine($"Digite a quantidade de soldados das Forças Farc");
+            int Quantidadefarc = int.Parse(Console.ReadLine());
+            List<int> Qtdes = new List<int>();
+            for (int i = 0; i < Quantidadefarc; i++)
+            {
+
+                Qtdes.Add(Quantidadefarc + 1);
+
+                Console.WriteLine("Digite a Patente e o Nome do soldadoFarc");
+                nsoldadofarc = Console.ReadLine();
+                Console.WriteLine("digite a idade ");
+                idadef = int.Parse(Console.ReadLine());
+                idadefarc.Add(idadef);
+
+
+
+            }
+        }
     }
 }
+public class Outros
+{
+
+    static string armasEequipamentos;
+    
+
+    public static List<int> preços = new List<int>();
+
+    public static void Outro()
+    {
+        Console.WriteLine("Quantas armas serao utilizadas");
+        List<int> Qtdea = new List<int>();
+        int armasadd = int.Parse(Console.ReadLine());
+        for (int i = 0; i < armasadd; i++)
+        {
+
+            Qtdea.Add(armasadd + 1);
+
+            Console.WriteLine("Adicione os armatentos e equipamentos desejados");
+            armasEequipamentos = Console.ReadLine();
+           //armamentosbr.Add(armasEequipamentos);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
